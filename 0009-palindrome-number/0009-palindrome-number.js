df803d16af2f3d68c2b46efeda07519ff42ff386
Vahe Ohanyan <115527223+OV111@users.noreply.github.com>
2025-10-21 14:30:50 +0400

    Time: 11 ms (26.4%), Space: 64.8 MB (47.76%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let reversed = String(x).split("").reverse().join("");
+  return x.toString() === reversed;
+};
