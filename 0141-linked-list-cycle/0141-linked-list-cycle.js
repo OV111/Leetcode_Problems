a194e71acb35715fb0f39c023811cb603089630f
Vahe Ohanyan <115527223+OV111@users.noreply.github.com>
2025-10-22 20:46:39 +0400

    Time: 68 ms (7.82%), Space: 59.4 MB (58.38%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+var hasCycle = function (head) {
+    let first = head
+    let second = head
+
+    while (second && second.next) {
+        first = first.next;
+        second = second.next.next
+        if (first === second) {
+            return true
+        }
+    }
+    return false;
+};
