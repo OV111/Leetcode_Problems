bda31ce685eab671980f2cfbdf010d75aedc4c8d
Vahe Ohanyan <115527223+OV111@users.noreply.github.com>
2025-12-18 15:25:00 +0400

    Time: 0 ms (100%), Space: 55.8 MB (16.83%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+    if (!s.length) return true;
+    if (!t.length) return false;
+    let i = 0;
+    for (let j = 0; j < t.length; ++j) {
+        if (s[i] === t[j]) {
+            ++i;
+        }
+    }
+    return i === s.length ? true : false;
+};
