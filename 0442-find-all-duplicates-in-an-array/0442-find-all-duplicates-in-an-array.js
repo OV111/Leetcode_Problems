2be824aac5de631eca74984175cc0be241d4eaf1
Vahe Ohanyan <115527223+OV111@users.noreply.github.com>
2025-12-18 11:24:00 +0400

    Time: 31 ms (29.8%), Space: 74.5 MB (32.78%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function (nums) {
+    const result = [];
+    const countObj = {};
+    for (let num of nums) {
+        if (countObj[num]) {
+            countObj[num] += 1;
+        } else {
+            countObj[num] = 1;
+        }
+    }
+    for (let key in countObj) {
+        if (countObj[key] > 1) {
+            result.push(Number(key));
+        }
+    }
+    return result;
+
+    // let result = [];
+    //   nums.sort((a,b) => a-b);
+
+    //   for(let i = 0; i < nums.length-1; ++i) {
+    //     if(nums[i] === nums[i+1]) {
+    //       result.push(nums[i]);
+    //     }
+    //   }
+    //   return result;
+};
