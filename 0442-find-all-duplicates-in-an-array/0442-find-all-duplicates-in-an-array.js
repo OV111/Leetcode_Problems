21c2a05aa3dab7ecf944eaf02ccfd017e01a652d
Vahe Ohanyan <115527223+OV111@users.noreply.github.com>
2025-12-18 11:24:48 +0400

    Time: 55 ms (10.99%), Space: 67.4 MB (60.52%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
@@ -3,29 +3,29 @@
  * @return {number[]}
  */
 var findDuplicates = function (nums) {
-    const result = [];
-    const countObj = {};
-    for (let num of nums) {
-        if (countObj[num]) {
-            countObj[num] += 1;
-        } else {
-            countObj[num] = 1;
-        }
-    }
-    for (let key in countObj) {
-        if (countObj[key] > 1) {
-            result.push(Number(key));
-        }
-    }
-    return result;
-
-    // let result = [];
-    //   nums.sort((a,b) => a-b);
-
-    //   for(let i = 0; i < nums.length-1; ++i) {
-    //     if(nums[i] === nums[i+1]) {
-    //       result.push(nums[i]);
+    // const result = [];
+    // const countObj = {};
+    // for (let num of nums) {
+    //     if (countObj[num]) {
+    //         countObj[num] += 1;
+    //     } else {
+    //         countObj[num] = 1;
     //     }
-    //   }
-    //   return result;
+    // }
+    // for (let key in countObj) {
+    //     if (countObj[key] > 1) {
+    //         result.push(Number(key));
+    //     }
+    // }
+    // return result;
+
+    let result = [];
+      nums.sort((a,b) => a-b);
+
+      for(let i = 0; i < nums.length-1; ++i) {
+        if(nums[i] === nums[i+1]) {
+          result.push(nums[i]);
+        }
+      }
+      return result;
 };
