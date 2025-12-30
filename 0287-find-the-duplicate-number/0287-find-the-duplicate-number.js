d71e7c24d42767378144feae25e7c205099f1d22
Vahe Ohanyan <115527223+OV111@users.noreply.github.com>
2025-12-30 14:45:29 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -2,14 +2,10 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findDuplicate = function(nums) {
+var findDuplicate = function (nums) {
     const set = new Set();
-    for(let num of nums) {
-        if(set.has(num)) {
-            return num;
-        } else {
-            set.add(num)
-        }
+    for (let num of nums) {
+        set.has(num) ? return num : set.add(num)
     }
     return -1
 };
